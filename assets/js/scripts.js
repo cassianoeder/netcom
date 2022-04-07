e8d4096759fa9cc57cfff5fbdc42bfd10d188a27
Ederson <wermeierederson@gmail.com>
2022-04-07 00:12:02 -0300

    primeiro commit do site

---
+++ assets/js/scripts.js
@@ -0,0 +1,9 @@
+const modal = document.querySelector('.modal__cobertura')
+
+function openModal() {
+  modal.classList.add('active')
+}
+
+function closeModal() {
+  modal.classList.remove('active')
+}
